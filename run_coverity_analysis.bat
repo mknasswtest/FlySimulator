e5e1c2cf242ccaabf471f3ce4c12e19980f23601
mknas <michal.knas@swtest.pl>
2025-10-02 15:11:03 +0200

    Update gitignore and add run coverity script

---
+++ run_coverity_analysis.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Ścieżka do narzędzia Coverity (zmień jeśli zainstalowane gdzie indziej)
+set COV_PATH="c:\Projects_training\cov-analysis"
+
+REM Ścieżka do projektu
+set PROJECT_PATH="c:\Projects_training\FlySimulator"
+
+REM Katalog wyjściowy analizy
+set OUTPUT_DIR="cov-int"
+
+REM Przejdź do katalogu projektu
+cd /d %PROJECT_PATH%
+
+REM Budowanie projektu z analizą Coverity
+%COV_PATH%\bin\cov-build.exe --dir %OUTPUT_DIR% gcc -c *.c
+
+REM Spakowanie wyników analizy
+tar -czvf flysimulator.tgz %OUTPUT_DIR%
+
+echo Analiza zakończona. Wyniki zapisane w flysimulator.tgz
+pause
